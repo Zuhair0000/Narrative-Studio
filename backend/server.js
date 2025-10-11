da54def939921c66bda603bd70aafcbe1a8a2efd
Zuhair Ahmed <153817334+Zuhair0000@users.noreply.github.com>
2025-10-11 22:51:05 +0800

    Backend setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const db = require("./db");
+const authRouter = require("./routes/authRoutes");
 
 const app = express();
 
@@ -9,6 +10,8 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 const PORT = process.env.PORT || 5000;
 
